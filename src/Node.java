96b70ebc8f97266cbb5e37f580817dc4cbfc8ecd
Derrick Galindo <derrickg@tamu.edu>
2020-11-06 19:04:52 -0600

    getters and setters for node

--- src/Node.java
+++ src/Node.java
@@ -4,14 +4,43 @@ public class Node {
     Node left;
     Node right;
     TreeFile file;
-   
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+
+    public Node(TreeFile file) {
+        this.key = null;
+        this.left = null;
+        this.right = null;
+    }
+
 
     public Node() {
         this.key = key;
         this.left = null;
         this.right = null;
-        this.file = null;
     }
-    
     
 }
