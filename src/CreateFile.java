45259e89398809c457a02f8b8a15893c5ff0e9b6
Derrick Galindo <derrickg@tamu.edu>
2020-11-25 16:46:08 -0600

    change name of filepath variable in CreateFile and added writeToFile method

--- src/CreateFile.java
+++ src/CreateFile.java
@@ -1,22 +1,35 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class CreateFile {
-    private String filename;
+    private String filepath;
     private ArrayList<String> userInput = new ArrayList<String>();
+    private FileWriter fileWriter;
 
     public CreateFile(String filename, ArrayList<String> userInput) throws IOException {
-        this.filename = filename;
+        this.filepath = filename;
         this.userInput = userInput;
 
         File newFile = new File(filename);
         try {
             newFile.createNewFile();
-            System.out.println("File added: " + this.filename);
+            System.out.println("File added: " + this.filepath);
         }
         catch (IOException e) {
-            System.out.println("Error occurred when creating " + this.filename);
+            System.out.println("Error occurred when creating " + this.filepath);
+        }
+    }
+
+    public void writeToFile() throws IOException {
+        try {
+            for (String input : userInput) {
+                fileWriter.write(input);
+            }
+        }
+        catch(IOException e) {
+            e.printStackTrace();
         }
     }
 }
